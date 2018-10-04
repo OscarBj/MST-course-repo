13b2db2b8888f18d796148347b21604562d4843a
Oscar Björkgren <osbjorkg@abo.fi>
2018-10-04 17:00:18 +0300

    lab 2 up to task 6

--- MST_lab2.ino
+++ MST_lab2.ino
@@ -1,11 +1,12 @@
-int sensorIN_1 = A0; // select the input pin for LDR
-int sensor1_Points = 0;
+int sensorIN_1 = A0; 
 int sensorIN_2 = A1;
-int sensor2_Points = 0;
 int sensorIN_3 = A3;
-int sensor3_Points = 0;
 int sensorIN_4 = A4;
-int sensor4_Points = 0;
+
+int reading_1 = 0;
+int reading_2 = 0;
+int reading_3 = 0;
+int reading_4 = 0;
 
 int windowSize = 100;
 
@@ -13,119 +14,122 @@ int * sensorBuf_1 = new int[windowSize];
 int * sensorBuf_2 = new int[windowSize];
 int * sensorBuf_3 = new int[windowSize];
 int * sensorBuf_4 = new int[windowSize];
-int * tmpArr = new int[4]; 
 int * sensorStateArr = new int[4];
-int * sensorPoints = new int[4];
+int * tmpArr = new int[4]; 
+int * multArr = new int[4];
 
-int treshold = 200;
+int treshold = 50; // what
 
 int idx = 0;
 
-int pair12 = 0;
-int pair13 = 0;
-int pair14 = 0;
-int pair23 = 0;
-int pair24 = 0;
-int pair34 = 0;
-
 void setup() {
-Serial.begin(9600); //sets serial port for communication
+Serial.begin(9600); // baud 9600
 }
 
 void loop() {
 if(idx>windowSize){
   
-  float var1 = calcVar(sensorBuf_1,windowSize,calcAverage(sensorBuf_1,windowSize));
+ // float var1 = calcVar(sensorBuf_1,windowSize,calcAverage(sensorBuf_1,windowSize));
   
-  float var2 = calcVar(sensorBuf_2,windowSize,calcAverage(sensorBuf_2,windowSize));
+ // float var2 = calcVar(sensorBuf_2,windowSize,calcAverage(sensorBuf_2,windowSize));
   
-  float var3 = calcVar(sensorBuf_3,windowSize,calcAverage(sensorBuf_3,windowSize));
+ // float var3 = calcVar(sensorBuf_3,windowSize,calcAverage(sensorBuf_3,windowSize));
   
-  float var4 = calcVar(sensorBuf_4,windowSize,calcAverage(sensorBuf_4,windowSize));
+ // float var4 = calcVar(sensorBuf_4,windowSize,calcAverage(sensorBuf_4,windowSize));
 
    idx = 0; 
 }
 
-sensorBuf_1[idx] = analogRead(sensorIN_1);
+reading_1 = analogRead(sensorIN_1);
+reading_2 = analogRead(sensorIN_2);
+reading_3 = analogRead(sensorIN_3);
+reading_4 = analogRead(sensorIN_4);
 
-sensorBuf_2[idx] = analogRead(sensorIN_2);
-
-sensorBuf_3[idx] = analogRead(sensorIN_3);
-
-sensorBuf_4[idx] = analogRead(sensorIN_4);
-
-  pair12 = abs(sensorBuf_1[idx]-sensorBuf_2[idx]);
-  if(pair12>treshold){
-    sensor1_Points++;
-    sensor2_Points++;
-  }
-  pair13 = abs(sensorBuf_1[idx]-sensorBuf_3[idx]);
-  if(pair13>treshold){
-    sensor1_Points++;
-    sensor3_Points++;
-  }
-  pair14 = abs(sensorBuf_1[idx]-sensorBuf_4[idx]);
-  if(pair14>treshold){
-    sensor1_Points++;
-    sensor4_Points++;
-  }
-  pair23 = abs(sensorBuf_2[idx]-sensorBuf_3[idx]);
-  if(pair23>treshold){
-    sensor2_Points++;
-    sensor3_Points++;
-  }
-  pair24 = abs(sensorBuf_2[idx]-sensorBuf_4[idx]);
-  if(pair24>treshold){
-    sensor2_Points++;
-    sensor4_Points++;
-  }
-  pair34 = abs(sensorBuf_3[idx]-sensorBuf_4[idx]);
-  if(pair34>treshold){
-    sensor3_Points++;
-    sensor4_Points++;
-  }
-
-  
-  for(int i=0;i<4;i++){
-    
+  if(abs(reading_1-reading_2)>treshold){
+	  sensorStateArr[0]++;
+	  sensorStateArr[1]++; 
   }
   
+  if(abs(reading_1-reading_3)>treshold){
+    sensorStateArr[0]++;
+	  sensorStateArr[2]++;
+  }
   
-  Serial.println(sensor1_Points);
-  Serial.println(sensor2_Points);
-  Serial.println(sensor3_Points);
-  Serial.println(sensor4_Points);
+  if(abs(reading_1-reading_4)>treshold){
+    sensorStateArr[0]++;
+	  sensorStateArr[3]++;
+  }
   
-  verifySensors();
+  if(abs(reading_2-reading_3)>treshold){
+    sensorStateArr[1]++;
+	  sensorStateArr[2]++;
+  }
   
-  resetState();
+  if(abs(reading_2-reading_4)>treshold){
+    sensorStateArr[1]++;
+	  sensorStateArr[3]++;
+  }
 
-tmpArr[0] = sensorBuf_1[idx];
-tmpArr[1] = sensorBuf_2[idx];
-tmpArr[2] = sensorBuf_3[idx];
-tmpArr[3] = sensorBuf_4[idx];
+  if(abs(reading_3-reading_4)>treshold){
+    sensorStateArr[2]++;
+	  sensorStateArr[3]++;
+  }
 
-int avg = calcAverage();
+tmpArr[0] = reading_1;
+tmpArr[1] = reading_2;
+tmpArr[2] = reading_3;
+tmpArr[3] = reading_4;
 
-//Serial.println(avg);
-//Serial.println(calcVar(tmpArr,4,avg));
+// calc average according to array with multipliers -> 0*sensor reading for a borken sensor anv 1*sensor reading for functioning sensor
+float avg = calcAverage(tmpArr,verifySensors(),4);
+Serial.println(avg);
+resetState();
+
+sensorBuf_1[idx] = reading_1;
+sensorBuf_2[idx] = reading_2;
+sensorBuf_3[idx] = reading_3;
+sensorBuf_4[idx] = reading_4;
 
 idx++;
 
 //sensorValue = analogRead(sensorPin); // read the value from the sensor
 //Serial.println(sensorValue); //prints the values coming from the sensor on the screen
 
-delay(100);
+delay(1000);
 // store values as integers (in buffer size = 100/sensor) conv to float when needed ( avg calc) 
 }
 
-void verifySensors(){
-  if(sensor1_Points == 2 && sensor2_Points == 2 && sensor3_Points == 2 && sensor3_Points == 2){
-    sensorStateArr = {1,1,1,1}; // special case check variance
+int* verifySensors(){
+
+  multArr[0] = 1;
+  multArr[1] = 1;
+  multArr[2] = 1;
+  multArr[3] = 1;
+  
+  if(sensorStateArr[0] == 2 && sensorStateArr[1] == 2 && sensorStateArr[2] == 2 && sensorStateArr[3] == 2){
+
+    Serial.println("weird state");
+      return multArr;
   }
-  if(sensor1_Points == 3){
-    sensorStateArr[0] = 0
+  
+  int oksensors = 0;
+  
+  for(int i=0;i<4;i++){
+    
+	  if(sensorStateArr[i] == 3){
+      Serial.print("Faulty sensor: ");
+		  Serial.println(i+1);
+		  multArr[i] = 0;
+   	}
+	  else{
+      oksensors++;
+      Serial.println(sensorStateArr[i]);
+	  }
   }
+  
+  Serial.print("oksensors: ");
+  Serial.println(oksensors);
+  return multArr;
 }
 
 float calcVar(int* a,int len, int avg){
@@ -139,27 +143,20 @@ float calcVar(int* a,int len, int avg){
  return val; 
 }
 
-float calcAverage(int* a,int len){
+// params: array with values to be calculated, array with multipliers for value array, lenght of arrays
+float calcAverage(int* a,int* multArr,int len){
   float val = 0;
+  int divider = 0;
   for(int i=0;i<len;i++){
-    val+=a[i];
+    val+=a[i]*multArr[i];
+    divider+=multArr[i];
   }
-  return float(val/len);
+  return float(val/divider);
 }
 
-void resetState(){
- 
-  pair12 = 0;
-  pair13 = 0;
-  pair14 = 0;
-  pair23 = 0;
-  pair24 = 0;
-  pair34 = 0;
-  
-  sensor1_Points = 0;
-  sensor2_Points = 0;
-  sensor3_Points = 0;
-  sensor4_Points = 0;
-  // reset buffers???????
+void resetState(){ 
+ sensorStateArr[0] = 0;
+ sensorStateArr[1] = 0;
+ sensorStateArr[2] = 0;
+ sensorStateArr[3] = 0;
 }
-// print avg for working ones and sensor not wokring
